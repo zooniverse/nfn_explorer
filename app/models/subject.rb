1832623c65ef949c9398b9c0d0c5152affe27a18
Stuart <stuart@zooniverse.org>
2014-12-03 14:54:53 -0500

    subject scopes

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,4 +2,9 @@ class Subject < ActiveRecord::Base
   belongs_to :collection
   has_many :annotations
   has_many :consensuses
+
+  scope :active,                  -> {where(state: "active")}
+  scope :complete,                -> {where(state: "complete")}
+  scope :complete_with_consensus, -> {where(state: "complete_with_consensus")}
+
 end
