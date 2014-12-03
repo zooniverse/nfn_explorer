de59bfb7e1d1269ed4884d5e905997491e12b6a2
Stuart <stuart@zooniverse.org>
2014-12-03 12:46:33 -0500

    set up annotations

--- app/models/annotation.rb
+++ app/models/annotation.rb
@@ -1,2 +1,4 @@
 class Annotation < ActiveRecord::Base
+  belongs_to :subject
+  belongs_to :consensus
 end
--- db/migrate/20141203152916_create_annotations.rb
+++ db/migrate/20141203152916_create_annotations.rb
@@ -1,7 +1,8 @@
 class CreateAnnotations < ActiveRecord::Migration
   def change
     create_table :annotations do |t|
-
+      t.string :type
+      t.hstore :fields
       t.timestamps
     end
   end
