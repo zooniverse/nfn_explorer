9a91e4986922d4d6402df9c38637f082fd731d0d
Stuart <stuart@zooniverse.org>
2014-12-03 14:54:47 -0500

    basic stats for a collection

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,3 +1,20 @@
 class Collection < ActiveRecord::Base
   has_many :subjects
+
+  def stats
+    {active: active_subject_count, complete: complete_subject_count, complete_with_consensus: complete_with_consensus_subject_count }
+  end
+
+  def active_subject_count
+    subjects.active.count
+  end
+
+  def complete_subject_count
+    subjects.complete.count
+  end
+
+  def complete_with_consensus_subject_count
+    subjects.complete_with_consensus.count
+  end
+
 end
