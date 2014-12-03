5c90b5bd468a02e228b3897a18541a9dc06aabcd
Stuart <stuart@zooniverse.org>
2014-12-03 12:46:42 -0500

    set up collections

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,2 +1,3 @@
 class Collection < ActiveRecord::Base
+  has_many :subjects
 end
--- db/migrate/20141203152846_create_collections.rb
+++ db/migrate/20141203152846_create_collections.rb
@@ -1,7 +1,12 @@
 class CreateCollections < ActiveRecord::Migration
   def change
     create_table :collections do |t|
-
+      t.string :name
+      t.string :image
+      t.text   :description
+      t.string :regions
+      t.string :ouroboros_id
+      t.string :url
       t.timestamps
     end
   end
