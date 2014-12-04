3d51771b9cd136de9688beac6c761754ac95140c
Stuart <stuart@zooniverse.org>
2014-12-04 09:16:07 -0500

    making this actually work

--- app/serializers/subject_serializer.rb
+++ app/serializers/subject_serializer.rb
@@ -1,6 +1,5 @@
 class SubjectSerializer < ActiveModel::Serializer
-  attributes :id, :image_url, :classification_count, :state, :external_id, :metadata, :collection_id, :created_at, :updated_at
+  attributes :id, :image_url, :classification_count, :state, :external_id, :metadata, :collection_id, :created_at, :updated_at, :annotations, :collection
   has_many :annotations
-  belongs_to :collection , only: [:name, :id, :image]
-
+  has_one  :collection
 end
