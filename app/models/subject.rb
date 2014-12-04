aa0ade60a3f308d2a692bb4b1cba40d14743f394
Stuart <stuart@zooniverse.org>
2014-12-04 15:53:44 -0500

    search scopes

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -7,6 +7,8 @@ class Subject < ActiveRecord::Base
   scope :complete,                -> {where(state: "complete")}
   scope :complete_with_consensus, -> {where(state: "complete_with_consensus")}
   scope :random,                  lambda {|count| order("RANDOM()").limit(count)}
+  scope :field_like,              lambda {|field, like| where("fields->? ILIKE ?",field,"%#{like}%" )}
+  scope :any_field,               lambda {|query| where("array_to_string(avals(fields), ',') ILIKE ? ", "%#{query}%")}
 
   def collect_annotations
     annotations.inject({}) do |r,a|
@@ -21,5 +23,14 @@ class Subject < ActiveRecord::Base
   def most_common_for_field(field)
     ranks =collect_annotations[field].inject({}){|r,a| r[a[:fields].values.first] ||=0;  r[a[:fields].values.first]+=1; r }
     ranks.sort{|a| a[1]}.last
+  end
+
+  def search (query)
+    if query.include? ":"
+      pairs = query.split(",").collect{|q| q.split(":")}
+    else
+      result = any_field(query)
+    end
+    result
   end
 end
