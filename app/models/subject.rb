fb6800b3ccc02faed5dd571523136ba003f55896
Stuart <stuart@zooniverse.org>
2014-12-04 09:17:23 -0500

    random and collect annotation methods

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -6,5 +6,9 @@ class Subject < ActiveRecord::Base
   scope :active,                  -> {where(state: "active")}
   scope :complete,                -> {where(state: "complete")}
   scope :complete_with_consensus, -> {where(state: "complete_with_consensus")}
+  scope :random,                  lambda {|count| order("RANDOM()").limit(count)}
 
+  def collect_annotations
+    annotations.inject({}){|r,a| r[a.name] ||= []; r[a.name]= {user_id: a.user_id, fields: a.fields} ; r}
+  end
 end
