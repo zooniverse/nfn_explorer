2cb9544adbdc52e1a29ec79696eb44557707717d
Stuart <stuart@zooniverse.org>
2014-12-04 14:11:24 -0500

    ignore in production

--- lib/Ouroboros.rb
+++ lib/Ouroboros.rb
@@ -1,7 +1,8 @@
 require 'mongo'
-require 'pry'
 
-config = YAML.load_file("#{Rails.root.to_s}/config/ouroboros.yml")[Rails.env]
-client = Mongo::MongoClient.new(config["host"], config["port"])
+if Rails.env =='development'
+  config = YAML.load_file("#{Rails.root.to_s}/config/ouroboros.yml")[Rails.env]
+  client = Mongo::MongoClient.new(config["host"], config["port"])
 
-Ouroboros     = client[config["db"]]
+  Ouroboros     = client[config["db"]]
+end 
