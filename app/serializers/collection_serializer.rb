f57871a7227edfc943d6fcdbadaa2f2ff5dd94cc
Stuart <stuart@zooniverse.org>
2014-12-04 09:18:10 -0500

    sample subjects on collection son response

--- app/serializers/collection_serializer.rb
+++ app/serializers/collection_serializer.rb
@@ -1,4 +1,8 @@
 class CollectionSerializer < ActiveModel::Serializer
-  attributes :id, :stats , :name, :image, :description, :regions, :external_id
+  attributes :id, :stats , :name, :image, :description, :regions, :external_id, :sample_subjects
 
+  def sample_subjects
+    object.subjects.random(10)
+  end
+  
 end
